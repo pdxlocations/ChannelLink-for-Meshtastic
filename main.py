51dc15cddd81e221040fae863a2186d31d57731f
pdxlocations <benlipsey@gmail.com>
2024-11-11 23:16:41 -0800

    import config 2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import logging
 import sys
 
-from config import BROKER, PORT
+import config
 from mqtt import create_mqtt_client
 from message_handler import on_message
 
@@ -10,9 +10,9 @@ def main():
     client = create_mqtt_client()
     client.on_message = on_message
 
-    logging.info(f"Connecting to broker at {BROKER}:{PORT}...")
+    logging.info(f"Connecting to broker at {config.BROKER}:{config.PORT}...")
     try:
-        client.connect(BROKER, PORT, keepalive=60)
+        client.connect(config.BROKER, config.PORT, keepalive=60)
     except Exception as e:
         print(f"Failed to connect to broker: {e}")
         sys.exit(1)
