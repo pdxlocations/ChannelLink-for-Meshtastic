d4bbb5164dda86f8c490bdf6b59d3a970dc8c28b
pdxlocations <benlipsey@gmail.com>
2024-11-16 20:08:13 -0800

    change to load_config

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import logging
 import sys
 
-import config
+import load_config
 from mqtt import create_mqtt_client
 from message_handler import on_message
 
@@ -10,9 +10,9 @@ def main():
     client = create_mqtt_client()
     client.on_message = on_message
 
-    logging.info(f"Connecting to broker at {config.BROKER}:{config.PORT}...")
+    logging.info(f"Connecting to broker at {load_config.BROKER}:{load_config.PORT}...")
     try:
-        client.connect(config.BROKER, config.PORT, keepalive=60)
+        client.connect(load_config.BROKER, load_config.PORT, keepalive=60)
     except Exception as e:
         print(f"Failed to connect to broker: {e}")
         sys.exit(1)
--- mqtt.py
+++ mqtt.py
@@ -1,11 +1,11 @@
 import paho.mqtt.client as mqtt
-import config
+import load_config
 from logger import logging
 
 def on_connect(client, userdata, flags, reason_code, properties=None):
     if reason_code == 0:
         logging.info("Connected to broker successfully")
-        for topic in config.TOPICS:
+        for topic in load_config.TOPICS:
             client.subscribe(topic + "/#")
             logging.info(f"Subscribed to topic: {topic}")
     else:
@@ -13,6 +13,6 @@ def on_connect(client, userdata, flags, reason_code, properties=None):
 
 def create_mqtt_client():
     client = mqtt.Client()
-    client.username_pw_set(config.USER, config.PASSWORD)
+    client.username_pw_set(load_config.USER, load_config.PASSWORD)
     client.on_connect = on_connect
     return client
